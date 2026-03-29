1a529da6d523d48e7bdb8a5902e6aa986ec4ee6f
invoker <p.zandi@digikala.com>
2026-03-30 01:31:29 +0330

    none

--- build.sh
+++ build.sh
@@ -1,49 +1,11 @@
-#!/usr/bin/env bash
-
-set -e
-
+#!/usr/bin/bash
 archs=(amd64 arm64)
-oses=(linux darwin windows)
+os=(linux darwin)
 
-APP_NAME="x-ui"
-
-mkdir -p dist
-
-for os in "${oses[@]}"; do
-  for arch in "${archs[@]}"; do
-
-    output_name="${APP_NAME}-${os}-${arch}"
-    build_path="dist/${output_name}"
-
-    echo "Building $output_name..."
-
-    # Windows needs .exe
-    if [ "$os" = "windows" ]; then
-      bin_name="${APP_NAME}.exe"
-    else
-      bin_name="${APP_NAME}"
-    fi
-
-    mkdir -p "$build_path"
-
-    # Build binary
-    GOOS=$os GOARCH=$arch go build -o "$build_path/$bin_name"
-
-    # Package
-    cd dist
-
-    if [ "$os" = "windows" ]; then
-      zip -r "${output_name}.zip" "$output_name"
-    else
-      tar -czf "${output_name}.tar.gz" "$output_name"
-    fi
-
-    # Cleanup folder (optional, like releases usually do)
-    rm -rf "$output_name"
-
-    cd - > /dev/null
-
-  done
+for o in ${os[@]}
+do
+for arch in ${archs[@]}
+do
+        env GOOS=${o} GOARCH=${arch} go build -o bin/x-ui_${o}_${arch}
 done
-
-echo "Done. Files are in ./dist"
+done
